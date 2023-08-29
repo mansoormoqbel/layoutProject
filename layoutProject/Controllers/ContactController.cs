aef1ef568fc838f2cd7f94d2b5fc75b277f52259
Home <mansoormoqbel124@gmail.com>
2023-08-29 23:04:16 +0300

    Add project files.

---
+++ layoutProject/Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace layoutProject.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Blog()
+        {
+            return View();
+        }
+        public IActionResult BlogDetails()
+        {
+            return View();
+        }
+        public IActionResult PortfolioDetails()
+        {
+            return View();
+        }
+        
+    }
+}
