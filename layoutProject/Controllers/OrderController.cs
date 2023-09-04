0351251945932020a2dbb8be87d5e901f97c9a03
MANSOOR MOQBEL <mansoormoqbel124@outlook.com>
2023-09-04 21:33:08 +0300

    mansoor update data

--- layoutProject/Controllers/OrderController.cs
+++ layoutProject/Controllers/OrderController.cs
@@ -15,34 +15,87 @@ namespace layoutProject.Controllers
     {
         SqlConnection con = new SqlConnection(@"Data Source= DESKTOP-360NCCM\MSSQLSERVER01; Integrated Security=true;Initial Catalog= DeliveryDB; ");
         SqlCommand cmd = new SqlCommand();
-        public IActionResult Index(Order CurrentOrder)
+        public IActionResult Index()
         {
-            //1- Open the Connetion to DB server
-            con.Open();
 
+            // Define the connection string and the query
+            
+            string query = "SELECT * FROM Orders";
 
-            cmd.Connection = con;
-            cmd.CommandText = "Insert Into Order() Values() ";
-            int RowAffected = cmd.ExecuteNonQuery();
-            if (RowAffected>0)
+            // Create a connection and a command objects
+            using (con)
             {
-                ViewBag.Message = "Record Inserted";
-            }
-            else
-            {
-                ViewBag.Message = "Error, Record not Inserted";
+                SqlCommand command = new SqlCommand(query, con);
+
+                // Open the connection and execute the query
+                con.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                // Pass the reader to the view using ViewBag
+                ViewBag.Reader = reader;
             }
 
-            // 5-close the con
-            con.Close();
-
-            ViewBag.Header = "Order Details";
-            ViewBag.MyOrder = CurrentOrder;
-            return View(CurrentOrder);
+            // Return the view
+            return View();
         }
         public IActionResult OrderRequest()
         {
             return View();
+        }
+        public IActionResult InsertOrder(Order CurrentOrder)
+        {
+            try
+            {
+                //1- Open the Connetion to DB server
+                con.Open();
+                cmd.Connection = con;
+                cmd.CommandText = "Insert Into Orders (OrderId, OrderKey, Phone, Email, Payment, Quantity, Price, Packaging, Tracking, Location, Address)" +
+                                    " Values (@OrderId, @OrderKey, @Phone, @Email, @Payment, @Quantity, @Price, @Packaging, @Tracking, @Location, @Address)";
+
+                cmd.Parameters.AddWithValue("@OrderId", CurrentOrder.OrderId);
+                cmd.Parameters.AddWithValue("@OrderKey", CurrentOrder.OrderKey);
+                cmd.Parameters.AddWithValue("@Phone", CurrentOrder.Phone);
+                cmd.Parameters.AddWithValue("@Email", CurrentOrder.Email);
+                cmd.Parameters.AddWithValue("@Payment", CurrentOrder.Payment);
+                cmd.Parameters.AddWithValue("@Quantity", CurrentOrder.Quantity);
+                cmd.Parameters.AddWithValue("@Price", CurrentOrder.Price);
+                cmd.Parameters.AddWithValue("@Packaging", CurrentOrder.Packaging);
+                cmd.Parameters.AddWithValue("@Tracking", CurrentOrder.Tracking);
+                cmd.Parameters.AddWithValue("@Location", CurrentOrder.Location);
+                cmd.Parameters.AddWithValue("@Address", CurrentOrder.Address);
+
+
+
+                //RowAffected
+                int RowAffected = cmd.ExecuteNonQuery();
+                if (RowAffected > 0)
+                {
+                    ViewBag.Message = "Record Inserted";
+                }
+                else
+                {
+                    ViewBag.Message = "Error, Record not Inserted";
+                }
+
+                
+               
+
+            }
+            catch(ArgumentNullException ex)
+            {
+                ViewBag.Message = ex.Message;
+            }
+            catch(Exception ex)
+            {
+                ViewBag.Message = ex.Message;
+                return View("Error",new ErrorViewModel());
+            }
+            finally
+            {
+                // 5-close the con
+                con.Close();
+            }
+            return View("Index");
         }
     }
 }
